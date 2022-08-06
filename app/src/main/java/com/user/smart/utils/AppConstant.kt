11d0b650868fa664d90851a68c21934aacdff393
Pr@WinK <pravin.kakde003@gmail.com>
2022-08-06 19:18:40 +0530

    Completed Dashboard raw design menu options

--- app/src/main/java/com/user/smart/utils/AppConstant.kt
+++ app/src/main/java/com/user/smart/utils/AppConstant.kt
@@ -2,5 +2,5 @@ package com.user.smart.utils
 
 object AppConstant {
     const val FROM_LOGIN_SCREEN_KEY = "key"
-    const val SPLASH_SCREEN_DELAY = 1000
+    const val SPLASH_SCREEN_DELAY = 2000
 }
