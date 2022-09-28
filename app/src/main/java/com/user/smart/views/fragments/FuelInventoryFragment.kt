56b0fc6febcc89cfdce6d60ba7e76de0b2c7b4f9
Pr@WinK <pravin.kakde003@gmail.com>
2022-09-28 12:22:12 +0530

    Added Pie Chart

--- app/src/main/java/com/user/smart/views/fragments/FuelInventoryFragment.kt
+++ app/src/main/java/com/user/smart/views/fragments/FuelInventoryFragment.kt
@@ -8,12 +8,21 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.user.smart.R
 import com.user.smart.databinding.FragmentFuelInventoryBinding
+import com.user.smart.utils.PreferenceManager
+import com.user.smart.utils.piechartutils.PieChart
+import com.user.smart.utils.piechartutils.Slice
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlin.random.Random
 
+@AndroidEntryPoint
 class FuelInventoryFragment : Fragment() {
 
     private var _binding: FragmentFuelInventoryBinding? = null
     private val binding get() = _binding!!
 
+    @Inject
+    lateinit var preferenceManager: PreferenceManager
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +35,33 @@ class FuelInventoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+
+        val pieChart = PieChart(
+            slices = provideSlices(), clickListener = null, sliceStartPoint = 0f, sliceWidth = 350f
+        ).build()
+
+        binding.fuelChart.setPieChart(pieChart)
+        binding.fuelChart.showLegend(binding.legendLayout)
+    }
+
+    private fun provideSlices(): ArrayList<Slice> {
+        return arrayListOf(
+            Slice(
+                Random.nextInt(100, 200).toFloat(),
+                R.color.primary_color,
+                "Regular"
+            ),
+            Slice(
+                Random.nextInt(100, 200).toFloat(),
+                R.color.red,
+                "Super"
+            ),
+            Slice(
+                Random.nextInt(100, 200).toFloat(),
+                R.color.materialIndigo600,
+                "Power"
+            )
+        )
     }
 
     private fun setupToolbar() {
@@ -41,6 +77,10 @@ class FuelInventoryFragment : Fragment() {
         binding.fuelInventoryToolbar.txtDashboardTitle.text =
             resources.getString(R.string.fuel_inventory)
         binding.fuelInventoryToolbar.toolbarParentCardView.elevation = 8f
+        val selectedStoreObject = preferenceManager.getSelectedStoreObject()
+        if (null != selectedStoreObject && !selectedStoreObject.store_name.isNullOrEmpty()) {
+            binding.txtStoreName.text = selectedStoreObject.store_name
+        }
     }
 
     override fun onDestroyView() {
