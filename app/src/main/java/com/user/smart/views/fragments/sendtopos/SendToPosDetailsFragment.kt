32194009a69fce18c74e519c3cced22fffa95219
Pr@WinK <pravin.kakde003@gmail.com>
2022-11-10 22:22:30 +0530

    Completed Design- Send to POS option

--- app/src/main/java/com/user/smart/views/fragments/sendtopos/SendToPosDetailsFragment.kt
+++ app/src/main/java/com/user/smart/views/fragments/sendtopos/SendToPosDetailsFragment.kt
@@ -7,51 +7,46 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.user.smart.R
-import com.user.smart.databinding.FragmentSendToPosBinding
+import com.user.smart.databinding.FragmentSendToPosDetailsBinding
 import com.user.smart.utils.PreferenceManager
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class SendToPosFragment : Fragment(), View.OnClickListener {
+class SendToPosDetailsFragment : Fragment() {
 
-    private var _binding: FragmentSendToPosBinding? = null
+    private var _binding: FragmentSendToPosDetailsBinding? = null
     private val binding get() = _binding!!
 
     @Inject
     lateinit var preferenceManager: PreferenceManager
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentSendToPosBinding.inflate(inflater, container, false)
+        _binding = FragmentSendToPosDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
-        setClickListener()
-    }
-
-    private fun setClickListener() {
-        binding.editButton.setOnClickListener(this)
     }
 
     private fun setupToolbar() {
-        binding.sendToPosToolbar.profileImage.setImageDrawable(
+        binding.sendToPosDetailsToolbar.profileImage.setImageDrawable(
             ContextCompat.getDrawable(
                 requireContext(),
                 R.drawable.ic_baseline_arrow_back_24
             )
         )
-        binding.sendToPosToolbar.imageViewProfile.setOnClickListener {
+        binding.sendToPosDetailsToolbar.imageViewProfile.setOnClickListener {
             requireActivity().onBackPressed()
         }
-        binding.sendToPosToolbar.txtDashboardTitle.text = resources.getString(R.string.send_to_pos)
-        binding.sendToPosToolbar.toolbarParentCardView.elevation = 8f
+        binding.sendToPosDetailsToolbar.txtDashboardTitle.text =
+            resources.getString(R.string.send_to_pos)
+        binding.sendToPosDetailsToolbar.toolbarParentCardView.elevation = 8f
         val selectedStoreObject = preferenceManager.getSelectedStoreObject()
         if (null != selectedStoreObject && !selectedStoreObject.store_name.isNullOrEmpty()) {
             binding.txtStoreName.text = selectedStoreObject.store_name
@@ -61,23 +56,5 @@ class SendToPosFragment : Fragment(), View.OnClickListener {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onClick(view: View?) {
-        when (view) {
-            binding.editButton -> {
-                val targetFragment = SendToPosDetailsFragment()
-                requireActivity().supportFragmentManager.beginTransaction()
-                    .setCustomAnimations(
-                        R.anim.trans_left_in,
-                        R.anim.trans_right_out,
-                        R.anim.trans_right_in, R.anim.trans_right_out
-                    )
-                    .add(R.id.fragmentContainer, targetFragment)
-                    .setReorderingAllowed(true)
-                    .addToBackStack("SendToPosDetailsFragment")
-                    .commit()
-            }
-        }
     }
 }
