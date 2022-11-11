006dc97c1f2dbdf4061c8382168677015165d209
Pr@WinK <pravin.kakde003@gmail.com>
2022-11-11 19:35:55 +0530

    Design- Account - Completed

--- app/src/main/java/com/user/smart/views/fragments/account/AccountFragment.kt
+++ app/src/main/java/com/user/smart/views/fragments/account/AccountFragment.kt
@@ -1,9 +1,12 @@
 package com.user.smart.views.fragments.account
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.user.smart.R
@@ -32,6 +35,18 @@ class AccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupToolbar()
+        setDropDownAdapter()
+    }
+
+    private fun setDropDownAdapter() {
+        val dropDownArrayList = resources.getStringArray(R.array.account_dropdown_array)
+        val adapter = ArrayAdapter(requireContext(), R.layout.dropdown_list_item, dropDownArrayList)
+        (binding.dropDownView.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        (binding.dropDownView.editText as? AutoCompleteTextView)?.setText(adapter.getItem(0), false)
+        (binding.dropDownView.editText as? AutoCompleteTextView)?.setOnItemClickListener { _, _, position, _ ->
+            val selectedTeam = adapter.getItem(position)
+            Log.e("TAGG", "" + selectedTeam)
+        }
     }
 
     private fun setupToolbar() {
